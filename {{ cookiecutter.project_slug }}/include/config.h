6c6182f7f1fffe0b3c4cc7a44badaa7f7a0df4eb
Norbert Szulc <norbert@not7cd.net>
2022-10-30 23:53:32 +0100

    You wouldn't get this from any other guy

--- {{ cookiecutter.project_slug }}/include/config.h
+++ {{ cookiecutter.project_slug }}/include/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define APPEND_MAC_TO_HOSTNAME
-const char* hostname = "trition-key-holder";
-const char* friendlyName = "Triton Key Holder";
-const char* mqttHost = "mqtt.hack";
+const char* hostname = "{{ cookiecutter.device_hostnome }}";
+const char* friendlyName = "{{ cookiecutter.device_friendly_name }}";
+const char* mqttHost = "{{ cookiecutter.device_mqtt_host }}";
